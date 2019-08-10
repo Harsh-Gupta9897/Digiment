a89fef0d8524f09fdc041805284844105a4198ca
vanshika lamba <vanshikalmb@gmail.com>
2019-08-10 17:03:01 +0530

    view pager

--- app/src/main/java/android/example/digiment/MainActivity.java
+++ app/src/main/java/android/example/digiment/MainActivity.java
@@ -1,13 +1,47 @@
 package android.example.digiment;
 
+import android.net.Uri;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  implements Tab1.OnFragmentInteractionListener , Tab2.OnFragmentInteractionListener,Tab3.OnFragmentInteractionListener , Tab4.OnFragmentInteractionListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TabLayout tabLayout = (TabLayout)findViewById(R.id.tabLayout);
+        tabLayout.addTab(tabLayout.newTab().setText("TAB 1"));
+        tabLayout.addTab(tabLayout.newTab().setText("TAB 2"));
+        tabLayout.addTab(tabLayout.newTab().setText("TAB 3"));
+        tabLayout.addTab(tabLayout.newTab().setText("TAB 4"));
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+        final ViewPager viewPager=(ViewPager)findViewById(R.id.pager);
+        final PagerAdapter adapter =new PagerAdapter(getSupportFragmentManager(),tabLayout.getTabCount());
+        viewPager.setAdapter(adapter);
+        viewPager.setOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
