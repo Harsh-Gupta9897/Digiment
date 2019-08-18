50d3eb3165e612680e6859d194aea33b0daf7e12
gvgh <sanchirastogi02@gmail.com>
2019-08-19 01:05:49 +0530

    ui added

--- app/src/main/java/android/example/digiment/SplashScreen.java
+++ app/src/main/java/android/example/digiment/SplashScreen.java
@@ -1,13 +1,33 @@
 package android.example.digiment;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class SplashScreen extends AppCompatActivity {
+import com.lusfold.spinnerloading.SpinnerLoading;
 
+public class SplashScreen extends AppCompatActivity {
+    private static int SPLASH_TIME_OUT =5000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+
+        SpinnerLoading view = findViewById(R.id.spin_load);
+        view.setPaintMode(1);
+        view.setCircleRadius(30);
+        view.setItemCount(8);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, Subject_dashboard.class);
+                startActivity(intent);
+                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+                finish();
+            }
+        },SPLASH_TIME_OUT);
     }
 }
---
+++ app/src/main/java/android/example/digiment/Subject_dashboard.java
@@ -0,0 +1,13 @@
+package android.example.digiment;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Subject_dashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_subject_dashboard);
+    }
+}
